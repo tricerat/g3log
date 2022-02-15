fa59807d2f462ec1a5802b47c1b7d5c45a26f942
Eric Musgrave <emusgrave@tricerat.com>
2022-02-15 10:59:09 -0800

    fix cmake config

--- cmake/g3logConfig.cmake
+++ cmake/g3logConfig.cmake
@@ -8,10 +8,10 @@
 #```
 # target_link_libraries(YourTarget PUBLIC g3log)
 #```
-# Variables and features 
+# Variables and features
 # ----------------------
-# * ``G3LOG`` -- if this environment variable is set, it'll be used as a hint as to where the g3log files are. 
-# * ``G3LOG_INCLUDE_DIRS`` -- raw cmake variable with include path 
+# * ``G3LOG`` -- if this environment variable is set, it'll be used as a hint as to where the g3log files are.
+# * ``G3LOG_INCLUDE_DIRS`` -- raw cmake variable with include path
 # * ``G3LOG_LIBRARIES`` -- raw cmake variable with library link line
 # * ``G3LOG_FOUND`` -- check if the lib was found without using the newer ``if(TARGET g3log)...``
 
@@ -27,7 +27,7 @@ if (NOT TARGET g3log)
 
    get_target_property(G3LOG_INCLUDE_DIR g3log INTERFACE_INCLUDE_DIRECTORIES)
    get_target_property(G3LOG_LIBRARY_DEBUG g3log IMPORTED_IMPLIB_DEBUG)
-   
+
    if (G3LOG_LIBRARY_DEBUG MATCHES ".*-NOTFOUND")
       get_target_property(G3LOG_LIBRARY_DEBUG g3log IMPORTED_LOCATION_DEBUG)
    endif ()
